957f27f43935b3572ac543f445c58aaa545050d5
Logesh Kumar <120043064+logeshk11@users.noreply.github.com>
2023-04-03 22:27:32 +0530

    Update capstone.py

--- capstone.py
+++ capstone.py
@@ -4,9 +4,9 @@ import random
 
 
 try:
-    data = pandas.read_csv("data/words_to_learn.csv")
+    data = pandas.read_csv("words_to_learn.csv")
 except FileNotFoundError:
-    data = pandas.read_csv("data/french_words.csv")
+    data = pandas.read_csv("french_words.csv")
 
 
 to_learn = data.to_dict(orient="records")
@@ -40,8 +40,8 @@ window.title("Capstone project")
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 
 canvers = Canvas(width=800, height=526)
-canvers_front_img = PhotoImage(file="images/card_front.png")
-canvers_back_img = PhotoImage(file="images/card_back.png")
+canvers_front_img = PhotoImage(file="card_front.png")
+canvers_back_img = PhotoImage(file="card_back.png")
 
 card_background = canvers.create_image(400, 263, image=canvers_front_img)
 canvers.grid(row=0, column=0, columnspan=2)
@@ -49,10 +49,10 @@ canvers.config(bg=BACKGROUND_COLOR, highlightthickness=0)
 card_title = canvers.create_text(400, 150, text="Title", font=("Ariel", 40, "italic"))
 card_word = canvers.create_text(400, 265, text="Word", font=("Ariel", 40, "bold"))
 
-cross_image = PhotoImage(file="images/wrong.png")
+cross_image = PhotoImage(file="wrong.png")
 wrong_button = Button(image=cross_image, highlightthickness=0, command=next_card)
 wrong_button.grid(row=1, column=0)
-tick_img = PhotoImage(file="images/right.png")
+tick_img = PhotoImage(file="right.png")
 tick_button = Button(image=tick_img, highlightthickness=0, command=known_word)
 tick_button.grid(row=1, column=1)
 next_card()
